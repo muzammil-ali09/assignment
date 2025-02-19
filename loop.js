9bfdcefb5c7ccf0f0c744b1347a62f1a8f6fc635
Muzammil Ali <muzuali295@gmail.com>
2025-02-19 10:34:27 +0500

    assignment-4

---
+++ loop.js
@@ -0,0 +1,111 @@
+
+// for (var i = 0; i < 5; i++) {
+//     document.write("Hello World<br>");
+// }
+
+
+
+        // for (let i = 1; i <= 10; i++) {
+        //     document.write(i + "<br>");
+        // }
+
+        
+
+// var A = ["Nokia", "Samsung", "Apple", "Sony", "Huawei"];
+
+
+// for (let i = 0; i < A.length; i++) {
+//     document.write(A[i] + '<br>'  );  
+// }
+
+
+// var html = `
+//     <h3>Students Scores Table</h3>
+//     <table border="1" style="width: 200px; text-align: center; border-collapse: collapse;">
+//         <tr>
+//             <th>Students</th>
+//             <th>Scores</th>
+//         </tr>
+// `;
+
+
+// var students = ["Ali", "Sami", "Taha", "Inam"];
+// var scores = [58, 73, 89, 90];
+
+
+// for (var i = 0; i < students.length; i++) {
+//     html += `
+//         <tr>
+//             <td>${students[i]}</td>
+//             <td>${scores[i]}</td>
+//         </tr>
+//     `;
+// }
+
+
+// html += `</table>`;
+
+// document.body.innerHTML = html;
+
+
+
+
+// var A = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+
+
+// var html = "<div style='font-size: 20px;'>";
+
+
+// for (var i = 0; i < A.length; i++) {
+    
+//     for (var j = 0; j < A[i].length; j++) {
+//         html += A[i][j] + " ";
+//     }
+//     html += "<br>"; 
+// }
+
+
+// html += "</div>";
+
+
+// document.body.innerHTML = html;
+
+
+
+
+
+// var num = parseFloat(prompt("Enter a number"));
+
+// // Initialize an empty string to store the output
+// var output = "";
+
+// // Loop to decrease the number by 0.5 each time until it reaches 0 or less
+// while (num >= 0) {
+//     output += num + ", ";
+//     num -= 0.5; // Decrease by 0.5
+// }
+// document.body.innerHTML = `<h3>Generated Sequence:</h3><p>${output.slice(0, -2)}</p>`;
+
+
+
+
+
+// var product = 1;
+
+
+// for (var i = 1; i <= 7; i += 2) {
+//     product *= i; 
+// }
+
+
+// document.body.innerHTML = `<h3>The product of the odd integers from 1 to 7 is ${product}</h3>`;
+
+
+
+
+
+
