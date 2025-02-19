9cc648d0dc68eed91c3a1ee6f0bed41c550c6908
Muzammil Ali <muzuali295@gmail.com>
2025-02-19 10:18:41 +0500

    assignment 1

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// var  userName= "Muzammil Ali";
+// var userAge ="17 year old";
+// var studentCourse= "Web and app development"; 
+
+
+
+// document.write(userName+userAge+studentCourse)
+
+
+// let age = 14; 
+
+// alert("I am " + age + " years old");
+
+
+// var a = 10, b = 20, c = 30;
