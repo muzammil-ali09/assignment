d0619959693b3948ae48b81f27080e120dc35cd2
Muzammil Ali <muzuali295@gmail.com>
2025-02-19 10:23:16 +0500

    assignment -3

---
+++ araray.js
@@ -0,0 +1,76 @@
+// var fruits = ["Apple", "Banana", "Cherry", "Mango"];
+
+// document.write(fruits)
+
+// var numbers = [10, 20, 30, 40, 50];
+// document.write(numbers)
+
+
+
+
+// var flags = [true, false, true, false, true];
+
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+
+// document.write("<h2>Qualifications:</h2>");
+
+//     document.write("<ul style-type=none>");
+// for (let i = 0; i < qualifications.length; i++) {
+//     document.write("<li>" + (i + 1) + ") " + qualifications[i] + "</li>");
+// }
+// document.write("</ul>");
+
+
+
+
+
+ 
+//  var topMovies = [];
+
+ 
+//  topMovies.push("taxali gate");
+//  topMovies.push("tere mari kahani");
+//  topMovies.push("khel khel main");
+//  topMovies.push("Dhoom 3");
+
+ 
+//  document.write("<h2><b>Top movies of 2019</b></h2>");
+//  document.write("<ul>");
+//  for (var i = 0; i < topMovies.length; i++) {
+//      document.write("<li>" + (i + 1) + ") " + topMovies[i] + "</li>");
+//  }
+//  document.write("</ul>");
+
+
+//  document.write("<h3>Length of the array: " + topMovies.length + "</h3>");
+
+
+// var favoriteCars = ["Audi", "Volvo", "Ford", "Lamborghini"];
+
+       
+//         var firstIndex = 0;
+//         var lastIndex = favoriteCars.length - 1;
+
+//         document.write("<h2><b>Favorite Cars</b></h2>");
+//         document.write(favoriteCars.join(", ") + "<br><br>");
+
+        
+//         document.write("First index of the array: " + firstIndex + "<br>");
+//         document.write("Car at first index of the array: " + favoriteCars[firstIndex] + "<br>");
+//         document.write("Last index of the array: " + lastIndex + "<br>");
+//         document.write("Car at last index of the array: " + favoriteCars[lastIndex]);'
+
+
+
+
+// var students = ["Michael", "John", "Tony"];
+// var scores = [320, 230, 480];
+// var totalMarks = 500;
+
+
+// document.write("<h2>Student Scores and Percentages</h2>");
+// for (var i = 0; i < students.length; i++) {
+//     var percentage = (scores[i] / totalMarks) * 100;
+//     document.write("Score of " + students[i] + " is " + scores[i] + ". Percentage: " + percentage + "%<br>");
+// }
