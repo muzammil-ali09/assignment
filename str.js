7768aeb28c5db3702aff1f39d779fd0e7a7c7e5a
Muzammil Ali <muzuali295@gmail.com>
2025-02-19 10:35:13 +0500

    assignment-5

---
+++ str.js
@@ -0,0 +1,77 @@
+// var userInput = prompt("Enter a word:");
+
+// var upperCaseInput = userInput.toUpperCase();
+
+// document.write("User input:", userInput);
+// document.write("Upper case:", upperCaseInput);
+
+
+
+// var userInput = prompt("Enter a word:");
+// var lowerCaseInput = userInput.toLowerCase();
+
+
+//  document.write("Lower case: " + lowerCaseInput);
+
+
+
+// var num = 35.36;
+// var strNum = num.toString().replace(".", "");
+// document.write("Number:", num);
+// document.write("Result:", strNum);
+
+// var a = "3";
+// var b = "3";
+// var x = a + b; 
+// document.write("a is", a, "<br>");
+// document.write("b is", b, "<br>");
+// document.write("a + b is", x, "<br>");
+
+
+// var a = "3";
+// var b = "3";
+// var x = a - b; 
+// document.write("a is", a, "<br>");
+// document.write("b is", b, "<br>");
+// document.write("a - b is", x, "<br>");
+
+// var university = "University of Karachi";
+// var charArray = university.split(""); 
+
+// for (var i = 0; i < charArray.length; i++) {
+//     document.write(charArray[i] + "<br>"); 
+// }
+
+// var userInput = prompt("Enter a word:");
+// var lastChar = userInput.charAt(userInput.length - 1); 
+
+// document.write("User input: " + userInput + "<br>");
+// document.write("Last character of input: " + lastChar);
+
+
+
+
+// var str =prompt("Enter any word")
+// str = str.toLowerCase(); 
+
+// var vowels = "aeiou";
+// var vowelCount = 0;
+// var consonantCount = 0;
+
+// for (var i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//         vowelCount++;
+//     } else if (str[i] >= 'a' && str[i] <= 'z') { 
+//         consonantCount++;
+//     }
+// }
+
+// document.write("String: " + str + "<br>");
+// document.write("Number of vowels: " + vowelCount + "<br>");
+// document.write("Number of consonants: " + consonantCount);
+
+
+
+
+
+
