da3aab4c0d3a007d5bd6cd93ab8c3d57331306ff
Muzammil Ali <muzuali295@gmail.com>
2025-03-02 20:20:47 +0500

    Add files via upload
    assignment 6 js

---
+++ assignment06.js
@@ -0,0 +1,117 @@
+// var num=+prompt("Enter any number")
+// var round=+prompt("Enter round off value")
+// var floor=+prompt("Enter floor value ")
+// var ceil=+prompt("Enter ceil value")
+// document.write(`number:  ${num} <br> round off value:  ${Math.round(round)} <br> floor value: ${Math.floor(floor)} <br> ceil value: ${Math.ceil(ceil)}`)
+
+
+// var usernum=+prompt("Enter any number")
+// var userroundoff=+prompt("Enter round off value")
+// var userfloor=+prompt("Enter floor value ")
+// var userceil=+prompt("Enter ceil value")
+// document.write(`number:  ${usernum} <br> round off value:  ${Math.round(userroundoff)} <br> floor value: ${Math.floor(userfloor)} <br> ceil value: ${Math.ceil(userceil)}`)
+
+
+// var number=+prompt("Enter any number")
+// var numberroundoff=+prompt("Enter round off value")
+// var numberfloor=+prompt("Enter floor value ")
+// var numberceil=+prompt("Enter ceil value")
+// document.write(`number:  ${number} <br> round off value:  ${Math.round(numberroundoff)} <br> floor value: ${Math.floor(numberfloor)} <br> ceil value: ${Math.ceil(numberceil)}`)
+
+
+
+// var pri=+prompt("Enter any number")
+// var pri2=+prompt("Enter round off value")
+// var pri3=+prompt("Enter floor value ")
+// var pri4=+prompt("Enter ceil value")
+// document.write(`number:  ${pri} <br> round off value:  ${Math.round(pri2)} <br> floor value: ${Math.floor(pri3)} <br> ceil value: ${Math.ceil(pri4)}`)
+
+
+// var user=prompt("Enter any value")
+// var final=Math.abs(user)
+//   document.write(`The absolute value of ${user} is ${final}`)
+
+// var user1=prompt("Enter head name ")
+// var user2=prompt("Enter tail name")
+// var toss=Math.random() *2
+// var ch =Math.floor 
+
+// if(ch==0){
+//     document.write(user1 +"win")
+// }else{
+//     document.write(user2 +"win")
+// }
+
+// var randomnum=Math.floor(Math.random()*100)+1;
+
+// document.write("random number between 1 to 100 is :",+randomnum)
+
+
+// var time=new Date()
+//  document.write(time)
+
+
+//  var month=["jan","feb","march","april","may","june","july","aug","sep","oct","nov","dec"]
+
+//  var priii=new Date().getMonth()
+
+//  document.write("Current Month is:" + month[priii]);   
+
+
+
+//  var week=["sun","mon","tue","wed","thu","fri","sat"]
+//  var priii=new Date().getDay()
+
+//  alert("Today is  " + week[priii]);
+
+
+
+
+//  var week=["sun","mon","tue","wed","thu","fri","sat"]
+
+//  var prii=new Date().getDay();
+
+//  if (prii === 0 || prii === 6){
+//     document.write("It's Fun day");
+//  }
+
+
+
+// var Time = new Date();
+
+
+// var milliseconds = Time.getTime();
+
+
+// var minutesSince = milliseconds / (1000 * 60);
+
+// document.write("Minutes since midnight, Jan 1, 1970: " + minutesSince);
+
+
+
+
+
+// var currentHour = new Date().getHours();
+
+
+// if (currentHour < 12) {
+//     document.write("It's AM");
+// } else {
+//     document.write("It's PM");
+// }
+
+
+
+
+
+// var age = prompt("Enter your age:");
+
+
+// var Year = new Date().getFullYear();
+
+
+// var birthYear = Year - age;
+
+
+// document.write( "Your age is " ,+ age ,"<br>" );
+// document.write( "your birth year is " ,+ birthYear )
